0db1d29f5eb57cbf4f3e7e6f670070b4d2740f76
Callum Yarnold <callum@yarnold.me>
2022-01-02 01:14:49 +0000

    Remove unused function and check if xattr was saved

--- src/file.rs
+++ src/file.rs
@@ -4,19 +4,6 @@ use std::fs;
 
 use xattr;
 
-fn get_xattr(xattr: &str, path: &str) -> String {
-	let mut xattrs = xattr::list(path).unwrap().peekable();
-	if xattrs.peek().is_none() {
-    return "".to_string()
-	}
-
-	for xattr in xattrs {
-		println!("{:?}", xattr)
-	}
-
-	"".to_string()
-}
-
 pub fn get_paste(paste_id: String) -> Result<paste::Paste, String> {
 	let path = format!("{}/{}", config::Config::load().paste_path, paste_id);
 	if let Ok(paste_contents) = fs::read_to_string(&path) {
@@ -31,8 +18,11 @@ pub fn get_paste(paste_id: String) -> Result<paste::Paste, String> {
 pub fn write_paste(paste: paste::Paste) -> Result<String, String> {
 	let path = format!("{}/{}", config::Config::load().paste_path, paste.id);
 	if let Ok(_resp) = fs::write(&path, paste.code) {
-		xattr::set(path, "language", paste.language.name.as_bytes());
-		Ok(paste.id)
+		if let Ok(_) = xattr::set(path, "language", paste.language.name.as_bytes()) {
+			Ok(paste.id)
+		} else {
+			Err("Can't set paste attributes".to_string())
+		}
 	} else {
 		Err("Can't save paste".to_string())
 	}
