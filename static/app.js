3d2994629dd0ecafd206f232dee725f786a44d06
Callum Yarnold <callum@yarnold.me>
2022-01-02 17:56:04 +0000

    Fix tab losing focus on textarea

--- static/app.js
+++ static/app.js
@@ -5,4 +5,29 @@ function UpdateLineNumbers() {
 
 $(document).ready(function() {
 	$('#code-editor').on('input', (event) => UpdateLineNumbers());
+
+	// https://stackoverflow.com/a/6140696
+	$("#code-editor").keydown(function(e) {
+		console.log("Here");
+	
+		if(e.keyCode === 9) { // tab was pressed
+				// get caret position/selection
+				var start = this.selectionStart;
+						end = this.selectionEnd;
+	
+				var $this = $(this);
+	
+				// set textarea value to: text before caret + tab + text after caret
+				$this.val($this.val().substring(0, start)
+										+ "\t"
+										+ $this.val().substring(end));
+	
+				// put caret at right position again
+				this.selectionStart = this.selectionEnd = start + 1;
+	
+				// prevent the focus lose
+				return false;
+		}
+	});
+	
 })
