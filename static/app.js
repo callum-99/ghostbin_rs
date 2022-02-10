bdd38f136e2417e88ab399669da832793cf00b26
Callum Yarnold <callum@yarnold.me>
2022-02-10 10:58:16 +0000

    Add paste decryption

--- static/app.js
+++ static/app.js
@@ -1,4 +1,4 @@
-function submit() {
+function encrypt() {
 	var key = document.getElementById('encryption').value;
 	var text = editor.getSession().getValue();
 
@@ -13,4 +13,27 @@ function submit() {
 	console.log(text);
 
 	document.getElementById('form-textarea').value = text;
+}
+
+function decrypt(key) {
+  
+  if(key == "") {
+    document.getElementById('crypto-error').innerHTML = "Empty password!";
+    return;
+  }
+
+  var cipher_text = editor.getSession().getValue();
+
+  var simpleCrypto = new SimpleCrypto(key);
+  try {
+    var plain_text = simpleCrypto.decrypt(cipher_text);
+    editor.getSession().setValue(plain_text);
+    console.log(plain_text);
+    document.getElementById('pass-form').hidden = true;
+    document.getElementById('code-editor').hidden = false;
+  }
+  catch(e) {
+    console.error(e);
+    document.getElementById('crypto-error').innerHTML = "Incorrect password!";
+  }
 }
